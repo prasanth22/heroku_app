26c791912cc850e7e61200aef512bfc34e16d636
prasanth22 <madduprasanth1@gmail.com>
2020-08-22 21:59:47 +0530

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Prasanth
                 </div>
 
                 <div class="links">
